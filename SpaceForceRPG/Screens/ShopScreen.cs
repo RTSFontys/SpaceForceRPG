f65f8952731c0f6f35dd70ef9e9a0854514435e9
Damian <Damian.meijer@gmail.com>
2020-05-26 11:36:23 +0200

    Screens

--- SpaceForceRPG/Screens/ShopScreen.cs
+++ SpaceForceRPG/Screens/ShopScreen.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace SpaceForceRPG
+namespace SpaceForceRPG.Screens
 {
-    public partial class Form1 : Form
+    public partial class ShopScreen : Form
     {
-        public Form1()
+        public ShopScreen()
         {
             InitializeComponent();
         }
